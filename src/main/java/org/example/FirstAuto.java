fe05d586beb16336d72ae5e6d1ca4d6f15cfa1a7
Madhan Gangadharan <madhan12322@gmail.com>
2024-02-13 13:48:34 +0530

    commit

---
+++ src/main/java/org/example/FirstAuto.java
@@ -0,0 +1,72 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirstAuto {
+    public static void main(String[] args) throws InterruptedException {
+        //WebDriver Driver =new ChromeDriver();
+//        Driver.get("https://www.youtube.com/");  //dry - dont reapeat yourself
+//        Driver.findElement(By.className("whsOnd zHQkBf"));
+//        Driver.findElement(By.id("identifierId"));
+//        Driver.findElement(By.tagName("input"));
+//        Driver.findElement(By.name("identifier"));
+//        Driver.findElement(By.partialLinkText("Madhan G"));
+//        Driver.findElement(By.linkText("Madhan"));
+//        Driver.findElement(By.cssSelector("input[type='email']")); //Important
+//        Driver.findElement(By.xpath("//input[@type='email')")); // important
+        //Driver.get("https://google.com/");
+        //Thread.sleep(5000);
+        //Driver.quit();
+//        WebDriver Driver = new FirefoxDriver();
+//        Driver.get("https://www.mozilla.org/en-US/firefox/products/");
+//
+//        Thread.sleep(5000);
+//        Driver.quit();
+
+//        ArrayList<String> names=new ArrayList<>();
+//
+//        names.add("Madhan");
+//        names.add("Gangadharan");
+//        names.add("G");
+//
+//        System.out.println(names.size());
+//
+//        for (int i=0;i<names.size();i++)
+//        {
+//            System.out.println(names.get(i));
+//            if (names.get(i)=="Madhan")
+//
+//            break;
+//
+//        }
+//
+//        for (String a:names)
+//        {
+//            System.out.println(a);
+//        }
+
+        String stringobject = "How are you today morning";
+
+        String[] Splittedstring=stringobject.split(" ");
+
+
+        System.out.println(Splittedstring.length);
+
+        for (int i=0;i<Splittedstring.length;i++){
+            System.out.println(Splittedstring[i]);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
